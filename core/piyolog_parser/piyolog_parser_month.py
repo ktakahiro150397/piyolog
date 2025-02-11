8dd9d3a4b990cc439ca33a158a5a56a1e481bf36
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2025-02-11 07:44:32 +0000

    月別データのパース処理

---
+++ core/piyolog_parser/piyolog_parser_month.py
@@ -0,0 +1,33 @@
+from datetime import date, datetime, timedelta
+import re
+from core.piyolog_parser.piyolog_parser_base import PiyoLogParserBase
+from core.piyolog_parser.piyolog_parser_day import PiyoLogParserDay
+from model.piyolog_day_record import PiyoLogDayRecord
+from model.piyolog_day_summary import PiyoLogDaySummary
+
+
+class PiyoLogParserMonth(PiyoLogParserBase):
+    """ぴよログファイルパーサー(月毎)"""
+
+    def __init__(self):
+        super().__init__()
+
+    def parse_file(self, file_path: str) -> list[PiyoLogDayRecord]:
+        # Implement this method in the subclass
+        raise NotImplementedError()
+
+    def parse_str(self, input_str: str) -> list[PiyoLogDayRecord]:
+
+        record_str_list = input_str.split("----------")
+
+        pattern = r"^\n\d{4}/\d{1,2}/\d{1,2}"
+        record_str_list = [
+            record_str.removeprefix("\n")
+            for record_str in record_str_list
+            if (record_str.strip() and re.match(pattern, record_str))
+        ]
+
+        day_parser = PiyoLogParserDay()
+        ret = [day_parser.parse_str(record_str) for record_str in record_str_list]
+
+        return ret
