43048e560a6927286dc59bc1b9d7942950a15263
Logic-Koike <koike@logic-biz.co.jp>
2025-02-25 05:47:29 +0000

    seleniumでの画像取得確認

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 import json
 
 from fastapi.responses import FileResponse
+from core.server.get_latest_pic import GetLatestPic
 from logger_factory import LoggerFactory
 
 logger = LoggerFactory.getLogger(__name__)
@@ -10,9 +11,21 @@ logger = LoggerFactory.getLogger(__name__)
 with open("server_settings.json") as f:
     settings = json.load(f)
 
-logger.info("url: %s",settings["url"])
-logger.info("width: %s",settings["width"])
-logger.info("height: %s",settings["height"])
+# Kindle PaperWhite 10 gen
+# 1072 × 1448
+# → 1072 x 1300 くらい？
+
+tempfilename = settings["tempfilename"]
+url = settings["url"]
+width = settings["width"]
+height = settings["height"]
+
+logger.info("tempfilename: %s",tempfilename)
+logger.info("url: %s",url)
+logger.info("width: %s",width)
+logger.info("height: %s",height)
+
+pic_getter = GetLatestPic(tempfilename)
 
 app = FastAPI()
 
@@ -23,7 +36,19 @@ async def read_root():
 @app.get("/latest_pic")
 async def latest_pic():
     """最新のURLの画像を取得して返します。"""
+    path = await pic_getter.get_latest_pic_from_url_async(url,width,height)
+    return FileResponse(str(path))
 
-    return FileResponse(f"{settings["tempfilename"]}")
+@app.get("/latest_pic2")
+def latest_pic2():
+    path = pic_getter.get_latest_pic_from_url(url,width,height)
+    return FileResponse(str(path))
+
+@app.get("/latest_pic_selenium")
+def latest_pic_selenium():
+    path = pic_getter.get_latest_pic_from_url_selenium(url,width,height)
+    return FileResponse(str(path))
+
+
 
 # uvicorn server:app --reload
